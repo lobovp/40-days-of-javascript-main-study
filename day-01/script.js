30599ab23ec00583551d9fc134a9d495854fb3bd
lobovp <petr.lobov@gmail.com>
2025-12-26 00:31:05 +0200

    first commit

---
+++ day-01/script.js
@@ -0,0 +1,4 @@
+console.log("Hello, tapaScript!");
+console.log("Welcome to 40 Days of JavaScript!");
+//document.write("Check the browser console for a message!");
+console.log("I am a test script!");
---
+++ day-01/test.js
@@ -0,0 +1,2 @@
+console.log("I am a test script!");
+document.getElementById("someID").innerText = "I am Some Div";
