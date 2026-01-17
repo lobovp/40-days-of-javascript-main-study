ed478ed2710dd3a081395d4b5133cb50fe8c5759
lobovp <petr.lobov@gmail.com>
2026-01-17 17:25:25 +0200

    day*03--4

--- day-03/index.js
+++ day-03/index.js
@@ -45,3 +45,15 @@ let ctc = annualSalary + bonus;
 console.log("Annual Salary:", annualSalary);
 console.log("Bonus:", bonus);
 console.log("CTC:", ctc);
+
+// ## 4. Write a program for the Traffic Light Simulation.
+
+// Red Light... Green Light... Let's Play!
+
+// - [ ] Create a `color` variable.
+// - [ ] Based on the color variable's value print in the console if a traveller needs to STOP or GO. The Red color is for STOP and the Green color is for GO.
+let color = "Green";
+console.log(
+  "Traffic Light:",
+  color === "Red" ? "STOP" : color === "Green" ? "GO" : "Invalid Color",
+);
