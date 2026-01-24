17f21d0dbc17588b9d5209719bbcd2d93ab70cfe
lobovp <petr.lobov@gmail.com>
2026-01-24 18:53:52 +0200

    day-04

---
+++ day-04/index.js
@@ -0,0 +1,21 @@
+console.log("Day 04: Advent of Code");
+let day = "Monday";
+
+switch (day) {
+  case "monday":
+    console.log("It's the start of the week.");
+    break;
+  default:
+    console.log("It's a normal day.");
+}
+// TASK 2
+let cash = 550;
+if (cash % 100 === 0) {
+  console.log("Withdrawal successful");
+} else {
+  console.log("Invalid amount");
+}
+// TASK 3
+let number1;
+let number2;
+
