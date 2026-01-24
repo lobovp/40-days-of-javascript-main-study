dfed6dfc73c698a9339dbd092d4a2b7cd1fedac8
lobovp <petr.lobov@gmail.com>
2026-01-24 19:02:52 +0200

    day-04

--- day-04/index.js
+++ day-04/index.js
@@ -16,6 +16,34 @@ if (cash % 100 === 0) {
   console.log("Invalid amount");
 }
 // TASK 3
+//Write a simple calculator that takes an operator (+, -, , /, %) as input,
+// and performs the operation on two numbers. Print the output on the console.
 let number1;
 let number2;
+let operator = "+";
+number1 = 20;
+number2 = 10;
 
+switch (operator) {
+  case "+":
+    console.log("Addition:", number1 + number2);
+    break;
+  case "-":
+    console.log("Subtraction:", number1 - number2);
+}
+// TASK4. Pay for your movie ticket
+// Imagine, the INOX charges ticket prices based on age:
+// - Children (<18 years): $3
+// - Adults (18 - 60 years): $10
+// - Seniors (60+ years): $8
+
+// Write a program that prints the ticket price based on the person’s age.
+let age = 5;
+
+if (age < 18) {
+  console.log("Ticket Price: $3");
+} else if (age >= 18 && age <= 60) {
+  console.log("Ticket Price: $10");
+} else {
+  console.log("Ticket Price: $8");
+}
